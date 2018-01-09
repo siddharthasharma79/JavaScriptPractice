17ba5b7a043556ae056a854bf0c116047a34a36f
siddharthasharma79 <siddharthasharma79@gmail.com>
2018-01-10 04:21:11 +0530

    added two Dimensional array example and sum of two array.

--- array.js
+++ array.js
@@ -1,5 +1,23 @@
 "use strict"
 
+// code to create 2 two diamentional array and sum of both Array
+var arr1 = getTwoDiamentionalArr(3,3);
+populateArr(arr1, 1, "asc");// populate array 1,2,3, .... 9
+document.write("<br/>");
+var arr2 = getTwoDiamentionalArr(3,3); // populate array 9,8,7....1
+populateArr(arr2, 9, "desc");
+
+// define two diamentional array to hold sum of arr1 and arr2
+var arrsum = getTwoDiamentionalArr(3,3);
+for( var i = 0; i < 3; i++ ) {
+  for( var j = 0; j < 3; j++ )
+    arrsum[i][j] = arr1[i][j] + arr2[i][j];
+}
+document.write("<br/>");
+printArr(arrsum);
+console.log("Sum of Array : \n" + arrsum);
+/*******************************************************************************************/
+
 // array splice example
 var arrsplice_add = [1,2,5,6];
 arrsplice_add.splice(2,0,3,4); // inserting 3,4 between 2 and 5
@@ -51,4 +69,40 @@ function printArray(array) {
     document.write(array[i] + "&emsp;")
 
   document.write("<br/><br/>");
+}
+
+//function to get two diamentional array
+function getTwoDiamentionalArr(rows = 3, columns =3) {
+  var arr = new Array(rows);
+  for( var i = 0; i < rows; i++ )
+    for( var j = 0; j < columns; j++ )
+      arr[i] = new Array(columns);
+  return arr;
+}
+
+// function to populate and print two diamentional Array
+function populateArr(array, start, order) {
+  var rows = array.length;
+  var colums = array[0].length;
+  for( var i = 0; i < rows; i++ ) {
+    for( var j = 0; j < colums; j++ ) {
+      array[i][j] = start;
+      document.write(start + "&emsp;");
+      start = (order == "asc" ? (start + 1) : (start -1));
+    }
+    document.write("<br/>");
+  }
+  console.log("populateArray() called : |\n" + array );
+}
+
+// function to print two diamentional Array
+function printArr(array) {
+  var rows = array.length;
+  var colums = array[0].length;
+  for( var i = 0; i < rows; i++ ) {
+    for( var j = 0; j < colums; j++ )
+      document.write(array[i][j] + "&emsp;");
+    document.write("<br/>");
+  }
+  console.log("printArr() called : \n" + array);
 }
